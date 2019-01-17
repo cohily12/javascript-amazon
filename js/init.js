11bbb7b4e1cb4320622289385eea703608ea1539
cohily12 <k96079123@gmail.com>
2019-01-17 16:57:42 +0900

    [Modify]local url variable name

--- js/init.js
+++ js/init.js
@@ -3,11 +3,11 @@ import { ajax } from "./module/ajax.js";
 
 const locationHref = location.href;
 
-ajax.getReq(`${localUrl}json/options.json`)
+ajax.getReq(`${locationHref}json/options.json`)
     .then(template.appendOptionHTML({ HTMLEl: document.querySelector("#select-category") }))
 
-ajax.getReq(`${localUrl}json/videoCarousel.json`)
+ajax.getReq(`${locationHref}json/videoCarousel.json`)
     .then(template.appendCarouselHTML({ HTMLEl: document.querySelector("#video-card ul") }))
 
-ajax.getReq(`${localUrl}json/musicCarousel.json`)
+ajax.getReq(`${locationHref}json/musicCarousel.json`)
     .then(template.appendCarouselHTML({ HTMLEl: document.querySelector("#music-card ul") }))
