47c76f59ce365cdcfb20f4e5a077efdbd27316b2
cohily12 <k96079123@gmail.com>
2019-01-27 23:19:18 +0900

    Update StickyLayer.js

--- js/module/StickyLayer.js
+++ js/module/StickyLayer.js
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
-=======
 import { $, $All} from "../util.js";
 
->>>>>>> 5889a2eeaef34772bd574a413556cd40058f33b0
 class StickyLayer{
     constructor( bindTo ){
         this.stickyEl = $(bindTo);
     }
 
-<<<<<<< HEAD
-    pinElement({ thresholdEl }){
-=======
     pinElement( selector ){
         const thresholdEl = $(selector);
->>>>>>> 5889a2eeaef34772bd574a413556cd40058f33b0
         let bPinEl = false;
 
         return _ => {
@@ -33,14 +26,9 @@ class StickyLayer{
         }
     }
 
-<<<<<<< HEAD
-    displayHiddenBar({ thresholdEl }){
-        const hiddenBar = this.stickyEl.querySelector(".nav-hidden-bar");
-=======
     displayHiddenBar( selector ){
         const thresholdEl = $(selector);
         const hiddenBar = $(".hidden-bar", this.stickyEl);
->>>>>>> 5889a2eeaef34772bd574a413556cd40058f33b0
         let bHiddenbar = false;
 
         return _ => {
@@ -61,18 +49,6 @@ class StickyLayer{
     }
 
     displayHiddenPlan(){
-<<<<<<< HEAD
-        const seeMoreBtn = this.stickyEl.querySelector(".bar-see-more .down-arrow");
-        const contentCloseBtn = this.stickyEl.querySelectorAll("#content-close-btn");
-        
-        seeMoreBtn.addEventListener("click", () => {
-            this.toggleHiddenEl();
-        });
-
-        contentCloseBtn.forEach(el => el.addEventListener("click", () => {
-            this.toggleHiddenEl();
-        }));
-=======
         const seeMoreBtn = $(".bar-see-more .down-arrow", this.stickyEl);
         const contentCloseBtn = $All("#content-close-btn", this.stickyEl);
         
@@ -94,29 +70,6 @@ class StickyLayer{
     run() {
         window.addEventListener("scroll", this.pinElement(".nav-lower"));
         window.addEventListener("scroll", this.displayHiddenBar(".prime-header-content .btn-prime-container"));
-        this.displayHiddenPlan();
->>>>>>> 5889a2eeaef34772bd574a413556cd40058f33b0
-    }
-
-    toggleHiddenEl(){
-        const hiddenBar = this.stickyEl.querySelector(".nav-hidden-bar");
-        const hiddenPlan = this.stickyEl.querySelector(".nav-hidden-plan");
-        const hiddenClose = this.stickyEl.querySelector(".nav-hidden-close");
-
-        hiddenBar.classList.toggle("hidden");
-        hiddenPlan.classList.toggle("visible");
-        hiddenClose.classList.toggle("visible");
-    }
-
-    run() {
-        window.addEventListener("scroll", this.pinElement({
-            thresholdEl: document.querySelector(".nav-lower")
-        }))
-
-        window.addEventListener("scroll", this.displayHiddenBar({
-            thresholdEl: document.querySelector(".prime-header-content .btn-prime-container")
-        }))
-
         this.displayHiddenPlan();
     }
 }
