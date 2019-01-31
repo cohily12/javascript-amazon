2e8cff9032828a7a992aac40dacc2d6397a2eaab
cohily12 <k96079123@gmail.com>
2019-01-31 16:02:56 +0900

    Update Autocomplete.js

--- js/module/Autocomplete.js
+++ js/module/Autocomplete.js
@@ -1,101 +1,108 @@
-import { template } from "./template.js";
 import { $, $All, network, debounce } from "../util.js"
+import { createOptionTemplate, createSuggestionTemplate, appendTemplate } from "./template.js";
 import { URL } from "../config.js"
 
 class Autocomplete {
-    constructor(target, { keywordsContainer, bDimmer }){
+    constructor(target, { keywordsContainer, acTime, bDimmer }){
+        if(bDimmer) this.dimmer = this.activeDim("#dimmer");
         this.searchEl = $(target);
         this.keywordsContainer = $(keywordsContainer);
-        if(bDimmer) this.dimmer = this.activeDim("#dimmer");
+        this.acTime = acTime;
+        this.init();
     }
 
-    run(){
-        this.searchEl.addEventListener("input", () => { 
-            if(!this.debouncer) this.debouncer = debounce(this.showKeywords.bind(this), 500);
-            this.debouncer();
-        })
-
-        this.searchEl.addEventListener("blur", () => {
-            this.removeKeywords();
-            this.dimmer(false);
-        })
+    init() {
+        this.searchEl.addEventListener("input", this.inputKeywords.bind(this));
+        this.searchEl.addEventListener("blur", this.removeKeywords.bind(this));
+        this.addOptionDOM();
     }
 
-    showKeywords(){
+    inputKeywords() {
+        if(!this.debouncer) this.debouncer = debounce(this.requestSuggestions.bind(this), this.acTime);
+        this.debouncer();
+    }
+    
+    async requestSuggestions(){
         if(this.searchEl.value === "") {
             this.removeKeywords();
-            this.dimmer(false);
             return;
         }
         
-        const keywordJson = network.get(`${URL.ACAPI}${this.searchEl.value}`);
+        const suggestionJson = await network.get(`${URL.ACAPI}${this.searchEl.value}`);
+        const suggestionTemplate = createSuggestionTemplate(suggestionJson);
+        
+        if(suggestionTemplate) this.showKeywords(suggestionTemplate);
+        else this.removeKeywords();
+    }
 
-        keywordJson
-            .then(template.appendSuggestionHTML(this.keywordsContainer))
-            .then(this.keypressEvent.bind(this))
-            .then(this.dimmer)
+    showKeywords(template) {
+        appendTemplate(this.keywordsContainer, template);
+        this.pressKey = this.setKeyEvent();
+        this.searchEl.addEventListener("keydown", this.pressKey);
+        if(this.dimmer) this.dimmer.on();
     }
 
     removeKeywords() {
         this.keywordsContainer.innerHTML = null;
+        this.searchEl.removeEventListener("keydown", this.pressKey);
+        if(this.dimmer) this.dimmer.off();
     }
 
-    keypressEvent(res) {
-        if(!res) return;
 
-        const suggestions = $All(".suggestion-link", this.keywordsContainer);
-        const helper = {
-            input: this.searchEl,
-            findTarget(){
-                if(currentId < 0) currentId = suggestions.length - 1;
-                if(currentId === suggestions.length) currentId = 0; 
-                
-                return suggestions[currentId];
-            },
-            activeHoverEffect(target) {
-                target.classList.add("hover");
-            },
-            removeHoverEffect(target) {
-                target.classList.remove("hover");
-            },
-            changeInput() {
-                this.input.value = res[currentId].value;
-            }
+    setKeyEvent() {
+        const suggestions = $All(".suggestion-link");
+        const key = {
+            arrowUp: 38,
+            arrowDown: 40,
+            enter: 13
+        }
+        const findTarget = (id) => {
+            if(id < 0) currentId = suggestions.length - 1;
+            if(id >= suggestions.length) currentId = 0; 
+            return suggestions[currentId];
         }
         let currentId = -1;
-    
-        this.searchEl.addEventListener("keydown", (evt) => {
-            if(evt.keyCode === 38) {
-                const [prevTarget, target] = [helper.findTarget(currentId), helper.findTarget(--currentId)];
-
-                helper.removeHoverEffect(prevTarget);
-                helper.activeHoverEffect(target);
-                helper.changeInput();
+        
+        return evt => {
+            if(evt.keyCode === key.arrowUp) {
+                const [prevTarget, target] = [findTarget(currentId), findTarget(--currentId)];
+                
+                prevTarget.classList.remove("hover");
+                target.classList.add("hover");
+                this.searchEl.value = suggestions[currentId].innerText;
             }
-            else if(evt.keyCode === 40) {
-                const [prevTarget, target] = [helper.findTarget(currentId), helper.findTarget(++currentId)];
-
-                helper.removeHoverEffect(prevTarget);
-                helper.activeHoverEffect(target);
-                helper.changeInput();
+            else if(evt.keyCode === key.arrowDown) {
+                const [prevTarget, target] = [findTarget(currentId), findTarget(++currentId)];
+                
+                prevTarget.classList.remove("hover");
+                target.classList.add("hover");
+                this.searchEl.value = suggestions[currentId].innerText;
             }
-            else if(evt.keyCode === 13) {
-                const target = helper.findTarget(currentId);
+            else if(evt.keyCode === key.enter) {
+                const target = findTarget(currentId);
                 evt.preventDefault();
                 target.click();
             }
-        })
-
-        return res;
+        }
     }
 
     activeDim(bindTo) {
         const targetEl = $(bindTo);
-
-        return command => {
-            if(command) targetEl.classList.add("show");
-            else targetEl.classList.remove("show");
+        return {
+            on() {
+                targetEl.classList.add("show");
+            },
+            off() {
+                targetEl.classList.remove("show");
+            }
         }
+    }
+
+    async addOptionDOM() {
+        const jsonData = await network.get(`${URL.SERVER}json/options.json`);
+        const optionTemplate = createOptionTemplate(jsonData);
+
+        appendTemplate($("#select-category"), optionTemplate);
     }
 }
 
