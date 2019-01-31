96eec930d5ce2b28cdaf69bf03fbe60409edcffb
cohily12 <k96079123@gmail.com>
2019-01-31 16:04:19 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,27 +2,24 @@ import { StickyLayer } from "./module/StickyLayer.js"
 import { Carousel } from "./module/Carousel.js";
 import { Autocomplete } from "./module/Autocomplete.js"
 
-const stickyLayer = new StickyLayer(".plan-layer");
-const videoCarousel = new Carousel("#video-card", {
-    intervalTime: 3000,
-    delayTime: 5000,
-    leftBtn: "#video-left",
-    rightBtn: "#video-right"
-});
-const musicCarousel = new Carousel("#music-card", {
-    intervalTime: 3000,
-    delayTime: 5000,
-    leftBtn: "#music-left",
-    rightBtn: "#music-right"
-});
-const autocomplete = new Autocomplete(".nav-input-bar", {
-    keywordsContainer: ".nav-search-autocomplete",
-    bDimmer: true
-});
+document.addEventListener("DOMContentLoaded", () => {
+    const stickyLayer = new StickyLayer(".plan-layer");
 
-window.addEventListener("load", () => {
-    stickyLayer.run();
-    videoCarousel.run();
-    musicCarousel.run();
-    autocomplete.run();
-});
+    const videoCarousel = new Carousel(".video-carousel", {
+        intervalTime: 3000,
+        delayTime: 5000,
+        itemUrl: "/videoCarousel.json"
+    });
+
+    const musicCarousel = new Carousel(".music-carousel", {
+        intervalTime: 3000,
+        delayTime: 5000,
+        itemUrl: "/musicCarousel.json"
+    });
+
+    const autocomplete = new Autocomplete(".nav-input-bar", {
+        keywordsContainer: ".nav-search-autocomplete",
+        acTime: 500,
+        bDimmer: true
+    });
+})
